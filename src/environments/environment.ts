ec1e00df1c9a5fd307337145fc0ac8147314c657
Gianfranco <gianjrs.98@gmail.com>
2024-06-09 17:04:49 +0200

    fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	// api: 'http://localhost:8000/api',
-	api: import.meta.env.NG_APP.API_URL,
-	apiKey: import.meta.env.NG_APP.API_KEY,
+	api: import.meta.env.NG_APP_API_URL,
+	apiKey: import.meta.env.NG_APP_API_KEY,
 	// baseUrl: import.meta.env.NG_APP.BASE_URL,
 	// version: import.meta.env.NG_APP_VERSION,
 };
