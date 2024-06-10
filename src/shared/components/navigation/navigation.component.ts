e9c551ee62fcf4e8e94b4937769b976de39fa04d
Gianfranco <gianjrs.98@gmail.com>
2024-06-10 21:17:33 +0200

    fix(app): final touches

--- src/shared/components/navigation/navigation.component.ts
+++ src/shared/components/navigation/navigation.component.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, WritableSignal } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { UserAuthenticatorFetcher } from '@app/modules/auth/fetchers/user-authentication-fetcher.service';
 import { UserAuthenticator } from '@app/modules/auth/services/user-authentication.service';
+import { UserAuthenticationSignalHandler } from '@app/modules/auth/signal-handlers/user-authentication-signal-handler.service';
+import { UserResponseDTO } from '@interfaces/user/user.interface';
 
 const providers = [UserAuthenticator, UserAuthenticatorFetcher];
 
@@ -18,7 +20,11 @@ export class NavigationComponent {
 	@Input() title: string;
 	isSidebarOpen = false;
 
-	constructor(private readonly userAuthenticator: UserAuthenticator) {}
+	public user: WritableSignal<UserResponseDTO> = this.userAuthSignalHandler.getUserAuthenticated();
+	constructor(
+		private readonly userAuthenticator: UserAuthenticator,
+		private readonly userAuthSignalHandler: UserAuthenticationSignalHandler,
+	) {}
 
 	toggleSidebar(): void {
 		this.isSidebarOpen = !this.isSidebarOpen;
